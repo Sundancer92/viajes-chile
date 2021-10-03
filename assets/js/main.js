d528a0aec3e528156526d7e2e0e867d5d336fc7e
Juan Pedro Tovar <jptovar92@gmail.com>
2021-10-03 00:09:43 -0300

    Typeform specs v8

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+// Declaracion de constantes
+//SVG Logo
+const planeta = document.getElementById("planeta");
+
+//Quienes Somos Cards (h5)
+const qshPlane = document.getElementById("qsh-plane");
+const qshMountain = document.getElementById("qsh-mountain");
+const qshRoute = document.getElementById("qsh-route");
+
+//Quienes Somos Cards (p)
+const qspPlane = document.getElementById("qsp-plane");
+const qspMountain = document.getElementById("qsp-mountain");
+const qspRoute = document.getElementById("qsp-route");
+
+var jetLag = false;
+
+// El avion rotara
+planeta.addEventListener("click",function () {
+    planeta.classList.toggle('que-vuele'); 
+});
+
+// Cambia algo de su P
+qshPlane.addEventListener("click",function () {
+    jetLag = !jetLag;
+  console.log(jetLag);
+  if (jetLag===true) {
+    alert('Tenes jet lag!');    
+} else {
+    alert('No tenes jet lag!');        
+}
+});
+
+
+qshMountain.addEventListener("click",function () {
+    qspMountain.classList.add('d-none'); 
+});
+qshRoute.addEventListener("click",function () {
+    qspRoute.classList.toggle('que-vuele'); 
+});
+
+
+
